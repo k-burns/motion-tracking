a01b6048180ba2849f269d8f29edfdae6db269c8
k-burns <katelynnburns655@gmail.com>
2023-08-01 15:12:26 -0400

    better contouring

--- lib/my_app/visual.ex
+++ lib/my_app/visual.ex
@@ -1,3 +1,2 @@
 defmodule MyApp.Visual do
-
 end
--- lib/my_app_web/controllers/page_html/home.ex
+++ lib/my_app_web/controllers/page_html/home.ex
@@ -4,13 +4,18 @@ defmodule MyAppWeb.HomeLive.Index do
 
   @impl true
   def mount(_params, _, socket) do
-    {:ok, socket
-    |> assign(running?: false)
-  |> assign(image: nil)
-  |> assign(prediction: nil)
-  |> assign(serving: serving())
-  |> assign(video: Evision.VideoCapture.videoCapture("/Users/katelynnburns/Documents/Zoom/2023-07-25 10.51.03 Katelynn (she_her)'s Zoom Meeting/video1536544321.mp4"))
-  }
+    {:ok,
+     socket
+     |> assign(running?: false)
+     |> assign(image: nil)
+     |> assign(prediction: nil)
+     |> assign(serving: serving())
+     |> assign(
+       video:
+         Evision.VideoCapture.videoCapture(
+           "/Users/katelynnburns/Documents/Zoom/2023-07-25 10.51.03 Katelynn (she_her)'s Zoom Meeting/video1536544321.mp4"
+         )
+     )}
   end
 
   @impl true
@@ -26,67 +31,72 @@ defmodule MyAppWeb.HomeLive.Index do
   def handle_info(:run, %{assigns: %{running?: true}} = socket) do
     frame = socket.assigns.video |> Evision.VideoCapture.read()
 
-   [predication, image] = predict(socket.assigns.serving, frame)
+    [predication, image] = predict(socket.assigns.serving, frame)
 
     send(self(), :run)
 
     {:noreply,
-    socket
-    |> assign(prediction: predication)
-    |> assign(image: Evision.imencode(".jpg", image) |> Base.encode64())}
+     socket
+     |> assign(prediction: predication)
+     |> assign(image: Evision.imencode(".jpg", image) |> Base.encode64())}
   end
 
   def handle_info(_msg, socket), do: {:noreply, socket}
 
   defp predict(serving, frame) do
     pred_tensor = frame |> Evision.Mat.to_nx() |> Nx.backend_transfer()
-    tensor = frame |> Evision.cvtColor(7) |> Evision.gaussianBlur({3, 3}, 0, SigmaY: 0)
-    canny = Evision.canny(tensor, 100, 200)
-    {contours, _} = Evision.findContours(canny, Evision.Constant.cv_RETR_LIST(), Evision.Constant.cv_CHAIN_APPROX_NONE())
+    tensor = frame |> Evision.cvtColor(7) |> Evision.gaussianBlur({7, 7}, 1)
+    canny = Evision.canny(tensor, 50, 190)
+    kernel = Evision.Mat.ones({3, 3}, :u8)
+    di = Evision.dilate(canny, kernel, iterations: 3)
+    closed = Evision.morphologyEx(di, Evision.Constant.cv_MORPH_CLOSE(), kernel)
 
-#     minimal_area = 100_000
+    {contours, _} =
+      Evision.findContours(
+        closed,
+        Evision.Constant.cv_RETR_LIST(),
+        Evision.Constant.cv_CHAIN_APPROX_NONE()
+      )
 
-# contours =
-#   Enum.reject(contours, fn c ->
-#     # Calculate the area of each contour
-#     area = Evision.contourArea(c)
-#     # Ignore contours that are too small or too large
-#     # (return true to reject)
-#     area < minimal_area
-#   end)
+    minimal_area = 100
 
-Enum.map(contours, fn c ->
-  IO.inspect(tensor)
-  # area = Evision.contourArea(c)
-  {x, y, w, h} = Evision.boundingRect(c)
-  cropped_img=tensor[y: y+h, x: x+w]
-  img_name= "#{x}.jpg"
-  Evision.imwrite(img_name, cropped_img)
-end) |> IO.inspect()
+    contours =
+      Enum.reject(contours, fn c ->
+        # Calculate the area of each contour
+        area = Evision.contourArea(c)
+        # Ignore contours that are too small or too large
+        # (return true to reject)
+        area < minimal_area
+      end)
 
-# IO.inspect("#{Enum.count(contours)} contour(s) remains")
+    # Enum.map(contours, fn c ->
+    #   # area = Evision.contourArea(c)
+    #   {x, y, w, h} = Evision.boundingRect(c)
+    #   cropped_img=tensor[y: y+h, x: x+w]
+    #   img_name= "#{x}.jpg"
+    #   Evision.imwrite(img_name, cropped_img)
+    # end)
 
-# color in {Blue, Green, Red}, range from 0-255
-edge_color = {0, 0, 255}
+    # IO.inspect("#{Enum.count(contours)} contour(s) remains")
 
-# # draw all contours by setting `index` to `-1`
-index = -1
+    # color in {Blue, Green, Red}, range from 0-255
+    edge_color = {0, 0, 255}
 
-# # Load image in color
+    # # draw all contours by setting `index` to `-1`
+    index = -1
 
-# # draw all contours on the color image
-image = Evision.drawContours(frame, contours, index, edge_color, thickness: 2)
+    # # Load image in color
 
-
-
+    # # draw all contours on the color image
+    image = Evision.drawContours(frame, contours, index, edge_color, thickness: 2)
     %{predictions: [%{label: label}]} = Nx.Serving.run(serving, pred_tensor)
 
     [label, image]
   end
 
   defp serving do
-    {:ok, model_info} = Bumblebee.load_model({:hf, "microsoft/resnet-50"})
-    {:ok, featurizer} = Bumblebee.load_featurizer({:hf, "microsoft/resnet-50"})
+    {:ok, model_info} = Bumblebee.load_model({:hf, "facebook/convnext-tiny-224"})
+    {:ok, featurizer} = Bumblebee.load_featurizer({:hf, "facebook/convnext-tiny-224"})
 
     Bumblebee.Vision.image_classification(model_info, featurizer,
       top_k: 1,
